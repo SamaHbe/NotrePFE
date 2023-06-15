e3912c104534072b069555b5b43e015dd59101b7
SamaHbe <samahbenabdallah793@gmail.com>
2023-06-15 16:26:35 +0200

    First Commit

---
+++ flutter_application_7/lib/laitages.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_7/Model/repas.dart';
+import 'package:flutter_application_7/laitages_controller.dart';
+
+class laitages extends StatefulWidget {
+  const laitages({super.key});
+
+  @override
+  _laitagesState createState() => _laitagesState();
+}
+
+class _laitagesState extends State<laitages> {
+  final LaitagesController _controller = LaitagesController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xff011638),
+        title: const Text(
+          "Créer mon repas ",
+          style:
+              TextStyle(fontSize: 18, color: Color.fromARGB(255, 252, 116, 25)),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(5.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              ..._controller.li.map((val) {
+                return SizedBox(
+                  width: double.infinity,
+                  height: 90,
+
+                  child: Card(
+                    elevation: 8,
+                    color: Colors.white,
+                    child: TextButton(
+                      // padding: const EdgeInsets.all(1.0),
+                      child: Column(
+                        children: <Widget>[
+                          ListTile(
+                            /*leading: Image.network(
+                                val.image,
+                              ),*/
+                            title: Text(
+                              val.name,
+                              style: const TextStyle(
+                                  fontSize: 15,
+                                  color: Color(0xff011638),
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, val.lien);
+                      },
+                    ),
+                  ),
+                  // ),
+                );
+              }).toList(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
